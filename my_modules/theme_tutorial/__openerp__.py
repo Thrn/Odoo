85c1f9ee2bae23d3625978d046ce6b7ecb0af3d3
infoteststg <infoteststg@gmail.com>
2016-09-03 15:04:04 -0400

    first theme

---
+++ my_modules/theme_tutorial/__openerp__.py
@@ -0,0 +1,11 @@
+{
+  'name':'My theme',
+  'description': 'My first theme',
+  'version':'0.1',
+  'author':'Thr',
+
+  'data': [
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
